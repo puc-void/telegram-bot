8785a96c5c30a248feaee2e54efe0d39d3d56f0c
puc-void <215346186+puc-void@users.noreply.github.com>
2026-05-26 09:47:05 +0600

    firebase env fix

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ FIREBASE SETUP
 ========================================
 */
 
-const serviceAccount = require('./serviceAccountKey.json')
-
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+
+    credential: admin.credential.cert({
+
+        projectId:
+            process.env.FIREBASE_PROJECT_ID,
+
+        clientEmail:
+            process.env.FIREBASE_CLIENT_EMAIL,
+
+        privateKey:
+            process.env.FIREBASE_PRIVATE_KEY
+                .replace(/\\n/g, '\n')
+
+    })
+
 })
 
 const db = admin.firestore()
@@ -359,7 +371,7 @@ Now send PDF file 👇`
                 {
                     caption:
 
-`📚 ${data.courseCode}
+                        `📚 ${data.courseCode}
 
 📄 ${data.questionType}
 
@@ -1235,7 +1247,7 @@ bot.action(
                 {
                     caption:
 
-`📚 ${data.courseCode}
+                        `📚 ${data.courseCode}
 
 📄 ${data.questionType}
 
